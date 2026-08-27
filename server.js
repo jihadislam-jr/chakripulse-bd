717a62baae28a62741be8bbb88e047e4ab94c0b4
Md. Jihadul Islam Jihad <jihadislam2677@gmail.com>
2026-08-27 17:47:21 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,23 @@
 const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Serve files from public folder
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`ChakriPulse BD running on port ${PORT}`);
+});
+const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
 const fs = require("fs");
